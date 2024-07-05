7d2d02ab104a171e8e69dbf4efd27e6303fbd55d
Anish K <akneni@gmail.com>
2024-07-05 17:43:26 -0400

    Add back AES encryption functionality

--- src/crypt_utils.rs
+++ src/crypt_utils.rs
@@ -149,3 +149,169 @@ pub mod hash_utils {
         hex::encode(hasher.finalize())
     }
 }
+
+pub mod aes_utils {
+    use ring::aead::{Aad, LessSafeKey, Nonce, UnboundKey, AES_256_GCM};
+    use ring::rand::{SecureRandom, SystemRandom};
+    use serde::{Serialize, Deserialize};
+    use anyhow::{anyhow, Result};
+
+    use super::hash_utils;
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct AesMsg {
+        pub data: Vec<u8>,
+        aad: Vec<u8>,
+        nonce: Option<Vec<u8>>,
+        pub is_encrypted: bool,
+    }
+    
+    impl AesMsg {
+        pub fn from_bytes(v: Vec<u8>) -> Self {
+            let aad = b"1523 Elizabeth Ave #130, Charlotte, NC 28204 (take a break from coding, eat some suhi!)".to_vec();
+    
+            AesMsg {
+                data: v,
+                aad: aad,
+                nonce: None,
+                is_encrypted: false,
+            }
+        }
+
+        pub fn from_str(s: &str) -> Self {
+            let plaintext = s.as_bytes().to_vec();
+            let aad = b"1523 Elizabeth Ave #130, Charlotte, NC 28204 (take a break from coding, eat some suhi!)".to_vec();
+    
+            AesMsg {
+                data: plaintext,
+                aad: aad,
+                nonce: None,
+                is_encrypted: false,
+            }
+        }
+
+        pub fn extract_str(&self) -> Result<String> {
+            if self.is_encrypted {
+                return Err(anyhow!("Error, message is still encypted"));
+            }
+
+            let s = self.data.clone();
+            String::from_utf8(s)
+                .map_err(|e| anyhow!("data is not in utf-8 -> {}", e))
+        }
+
+    }
+    
+    pub struct AesEncryption {
+        key: [u8; 32]
+    }
+    
+    impl AesEncryption {
+        pub fn new() -> Self {
+            let rng = SystemRandom::new();
+            let mut key = [0_u8; 32];
+            rng.fill(&mut key)
+                .unwrap();
+            
+            AesEncryption{
+                key: key
+            }
+        }
+
+        pub fn from (key: &[u8; 32]) -> Self {
+            AesEncryption{
+                key: key.clone()
+            }
+        }
+        
+        pub fn from_tyb_apikey(apikey: impl AsRef<[u8]>) -> Self {
+            let mut key = [0_u8; 32];
+            for (i, c) in hash_utils::sha256(apikey).chars().enumerate() {
+                if i >= 32 {
+                    break;
+                }
+                key[i] = c as u8;
+            }
+
+            AesEncryption {
+                key: key
+            }
+        }
+
+        pub fn encrypt(&self, msg: &mut AesMsg) -> Result<(), String> {
+            let rng = SystemRandom::new();
+    
+            let mut in_out = if !msg.is_encrypted {
+                msg.data.clone()
+            }
+            else {
+                return Err("message is already encrypted".to_string());
+            };
+    
+            // Prepare the key
+            let unbound_key = match UnboundKey::new(&AES_256_GCM, &self.key) {
+                Ok(k) => k,
+                Err(e) => return Err(format!("Failed to create new key: {}", e)),
+            };
+            let less_safe_key = LessSafeKey::new(unbound_key);
+        
+            // Generate a unique nonce for this operation
+            let mut nonce_bytes = vec![0u8; 12]; // 96 bits for AES-GCM nonce
+            rng.fill(&mut nonce_bytes).unwrap();
+            msg.nonce = Some(nonce_bytes.clone());
+        
+            let nonce = Nonce::try_assume_unique_for_key(&nonce_bytes).unwrap();
+        
+            // The buffer where the in-place operation will happen
+            let tag = less_safe_key.seal_in_place_separate_tag(nonce, Aad::from(msg.aad.clone()), &mut in_out).unwrap();
+        
+            // Append the tag to the ciphertext
+            in_out.extend_from_slice(tag.as_ref());
+        
+    
+            msg.data = in_out;
+            msg.is_encrypted = true;
+            Ok(())
+        }
+    
+        pub fn decrypt(&self, msg: &mut AesMsg) -> Result<(), String> {
+            // Prepare the key
+            let unbound_key = UnboundKey::new(&AES_256_GCM, &self.key).unwrap();
+            let less_safe_key = LessSafeKey::new(unbound_key);
+    
+            let ciphertext = if msg.is_encrypted {
+                msg.data.clone()
+            }
+            else {
+                return Err("no ciphertext in message".to_string());
+            };
+            let nonce = if let Some(nonce) = msg.nonce.clone() {
+                msg.nonce = None;
+                nonce
+            }
+            else {
+                return Err("no nonce in message".to_string());
+            };
+            let aad = msg.aad.clone();
+    
+        
+            // Convert the nonce back to a `Nonce` type
+            let nonce = Nonce::try_assume_unique_for_key(nonce.as_slice()).unwrap();
+        
+            // Separate the actual ciphertext from the tag appended at the end
+            // The tag is the last 16 bytes of the input provided for AES-256 GCM
+            let tag_len = AES_256_GCM.tag_len();
+            let (ciphertext, tag) = ciphertext.split_at(ciphertext.len() - tag_len);
+        
+            let mut in_out = ciphertext.to_vec();
+            // Append the tag to be able to verify it during decryption
+            in_out.extend_from_slice(tag);
+        
+            // Decrypt in place, which also verifies the tag
+            let plaintext = less_safe_key.open_in_place(nonce, Aad::from(aad), &mut in_out).unwrap();
+            msg.data = plaintext.to_vec();
+            msg.is_encrypted = false;
+            Ok(())
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,12 @@ pub mod constants;
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crypt_utils::compression_utils;
+    use crypt_utils::{compression_utils, aes_utils};
+    use std::{
+        path::Path,
+        fs,
+    };
+
     
     #[test]
     fn compression() {
@@ -20,7 +25,10 @@ mod tests {
             .expect("error");
 
         let s = format!("Compressed Data: {:?}\nSize: {}", &packet, packet.data.len());
-        std::fs::write("./test-outputs/encrypted-data.txt", &s)
+        if !Path::new("./test-outputs").exists() {
+            fs::create_dir("./test-outputs").unwrap();
+        }
+        fs::write("./test-outputs/encrypted-data.txt", &s)
             .unwrap();
 
         compression_utils::decompress_brotli(&mut packet)
@@ -38,9 +46,32 @@ mod tests {
     #[test]
     fn apikey_generation() {
         let key = crypt_utils::gen_apikey("keys", "salt");
+        if !Path::new("./test-outputs").exists() {
+            fs::create_dir("./test-outputs").unwrap();
+        }
         std::fs::write("./test-outputs/out.txt", &key).unwrap();
         assert!(key.starts_with("tyb_key_"));
         assert!(key.len() > 64);
+    }
+
+    #[test]
+    fn aes_keygen() {
+        let key = crypt_utils::gen_apikey("keys", "salt");
+        let aes = aes_utils::AesEncryption::from_tyb_apikey(&key);
+
+        let plaintext = "hey!!";
+        let mut msg = aes_utils::AesMsg::from_str(&plaintext);
+
+        aes.encrypt(&mut msg)
+            .unwrap();
+
+        let mut msg2 = msg.clone();
+
+        aes.decrypt(&mut msg2).unwrap();
+
+        let res: String = msg2.extract_str().unwrap();
+
+        assert_eq!(res, plaintext);       
     }
 
 }

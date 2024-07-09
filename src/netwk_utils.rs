a0a48509aca1344393744e8dcabd04a8f17a2f2c
Anish Kanthamneni <109305222+akneni@users.noreply.github.com>
2024-07-09 19:25:31 -0400

    Minor change

--- src/netwk_utils.rs
+++ src/netwk_utils.rs
@@ -1,6 +1,6 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PubAddr {
     pub node_id: String,
     pub email: String,

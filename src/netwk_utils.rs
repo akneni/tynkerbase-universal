241e3633181291c79741e47ea9f5d26bdffac8a7
Anish Kanthamneni <109305222+akneni@users.noreply.github.com>
2024-07-28 14:55:10 -0400

    Add project config to universal

--- src/netwk_utils.rs
+++ src/netwk_utils.rs
@@ -47,3 +47,13 @@ impl NodeDiags {
         }
     }
 }
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct ProjConfig {
+    pub proj_name: String,
+    pub node_names: Vec<String>,
+    pub port_mapping: Vec<[u16; 2]>,           // [host port, container port]
+    pub volume_mapping: Vec<[String; 2]>,      // [host directory, container directory]
+    pub ignore: Vec<String>,
+}     
+
